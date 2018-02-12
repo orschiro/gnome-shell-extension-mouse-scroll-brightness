bf506c690f2d48df030e37b59ce6e4ed8624a8af
Robert Orzanna <robert@orzanna.de>
2018-02-12 15:59:05 +0100

    defined functions and variables

--- extension.js
+++ extension.js
@@ -1,6 +1,12 @@
-// define battery icon
+// define power icon
+const Power = imports.ui.status.power;
+
+// define scroll events
+let _onPowerIndicatorScrollEventId;
 
 // if detect mouse scroll up event over battery icon
+function enable() {
+  _onPowerIndicatorScrollEventId = powerIndicator.indicators.connect('scroll-event', ?);
 
 // increase brightness
 // show visual notification of brightness change

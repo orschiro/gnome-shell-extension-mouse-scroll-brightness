050bdf23fc0b65bef68446c39d61786c381ad663
Robert Orzanna <robert@orzanna.de>
2018-02-12 15:51:26 +0100

    designed logic

---
+++ extension.js
@@ -0,0 +1,13 @@
+// define battery icon
+
+// if detect mouse scroll up event over battery icon
+
+// increase brightness
+// show visual notification of brightness change
+
+// if detect mouse scroll down event over battery icon
+// decrease brightness
+// show visual notification of brightness change
+
+
+

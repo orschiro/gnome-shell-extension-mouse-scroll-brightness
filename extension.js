f41a352170a5284633b26654371e60ddcacc1195
Robert Orzanna <robert@orzanna.de>
2018-02-14 14:38:31 +0100

    Adapted volume.js

--- extension.js
+++ extension.js
@@ -1,19 +1,51 @@
-// define power icon
-const Power = imports.ui.status.power;
+// adapting from https://gitlab.gnome.org/GNOME/gnome-shell/blob/master/js/ui/status/volume.js#L316
+const Clutter = imports.gi.Clutter;
+const Lang = imports.lang;
+const Gio = imports.gi.Gio;
+const Gvc = imports.gi.Gvc;
+const St = imports.gi.St;
+const Signals = imports.signals;
 
-// define scroll events
-let _onPowerIndicatorScrollEventId;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const Slider = imports.ui.slider;
 
-// if detect mouse scroll up event over battery icon
-function enable() {
-  _onPowerIndicatorScrollEventId = powerIndicator.indicators.connect('scroll-event', ?);
+var Indicator = new Lang.Class({
+    Name: 'PowerIndicator',
+    Extends: PanelMenu.SystemIndicator,
 
-// increase brightness
-// show visual notification of brightness change
+    _init: function() {
+        this.parent();
 
-// if detect mouse scroll down event over battery icon
-// decrease brightness
-// show visual notification of brightness change
+        this._primaryIndicator = this._addIndicator();
 
+        this._control = getPowerControl();
+        this._powerMenu = new PowerMenu(this._control);
+        this._powerMenu.connect('icon-changed', Lang.bind(this, function(menu) {
+            let icon = this._powerMenu.getIcon();
 
+            if (icon != null) {
+                this.indicators.show();
+                this._primaryIndicator.icon_name = icon;
+            } else {
+                this.indicators.hide();
+            }
+        }));
 
+        this.menu.addMenuItem(this._powerMenu);
+
+        this.indicators.connect('scroll-event', Lang.bind(this, this._onScrollEvent));
+    },
+
+    _onScrollEvent: function(actor, event) {
+        let result = this._powerMenu.scroll(event);
+        if (result == Clutter.EVENT_PROPAGATE || this.menu.actor.mapped)
+            return result;
+
+        let gicon = new Gio.ThemedIcon({ name: this._powerMenu.getIcon() });
+        let level = this._powerMenu.getLevel();
+        Main.osdWindowManager.show(-1, gicon, null, level);
+        return result;
+    }
+});                                                                  

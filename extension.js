a32f9d0d601e7325f4fa882403bb6835dcf057ef
Robert Orzanna <robert@orzanna.de>
2018-02-14 18:14:46 +0100

    updated functions

--- extension.js
+++ extension.js
@@ -10,7 +10,6 @@ Compare with volume icon scroll to change volume
 
 // adapting from https://gitlab.gnome.org/GNOME/gnome-shell/blob/master/js/ui/status/volume.js#L316
 
-
 // init, enable, disable functions
 // And disconnect in disable, of course
 function init() {
@@ -18,22 +17,21 @@ function init() {
 }
 
 function enable() {
-	_onScrollEvent: function(actor, event) {
-		let result = this._powerMenu.scroll(event);
-		if (result == Clutter.EVENT_PROPAGATE || this.menu.actor.mapped)
-		    return result;
-
-		let gicon = new Gio.ThemedIcon({ name: this._powerMenu.getIcon() });
-		let level = this._powerMenu.getLevel();
-		Main.osdWindowManager.show(-1, gicon, null, level);
-		return result;
-	    }
+	// https://github.com/julio641742/gnome-shell-extension-reference/blob/master/REFERENCE.md#powerjs
+	panelMenu.SystemIndicator.indicators.connect(_onScrollEvent);	
 }
 
 function disable() {
-	powerIndicator.indicators.disconnect();
+	panelMenu.SystemIndicator.indicators.disconnect();
 }
 
+_onScrollEvent: function(actor, event) {
+		let result = this._panelMenu.SystemIndicator.scroll(event);
+		if (result == Clutter.EVENT_PROPAGATE || this.menu.actor.mapped)
+		    return result;
 
-
-
+		let gicon = new Gio.ThemedIcon({ name: this._panelMenu.SystemIndicator.getIcon() });
+		let level = this._panelMenu.SystemIndicator.getLevel();
+		Main.osdWindowManager.show(-1, gicon, null, level);
+		return result;
+}
